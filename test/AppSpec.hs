a15b36278ed0505e78e468eb4777310ef5a0d88e
Mateu Adsuara <mateuadsuara@gmail.com>
2021-01-25 19:53:03 +0100

    Fix and clean tests

--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -2,22 +2,25 @@
 module AppSpec(spec) where
 
 import Lib (app, Estimation(..))
-import Test.Hspec
-import Test.Hspec.Wai
-import Test.Hspec.Wai.JSON
+
 import Data.Aeson
-import Network.HTTP.Types.Header
 import Data.ByteString.Lazy
 import qualified Codec.Binary.UTF8.String as C
-
-main :: IO ()
-main = hspec spec
+import Network.Wai.Test
+import Test.Hspec
+import Test.Hspec.Wai
 
 toS :: ByteString -> String
 toS = C.decode . unpack
 
-withBody :: ByteString -> MatchBody
-withBody expected = MatchBody $ \_ body -> if body == expected then Nothing else Just $ "Got: " ++ (toS body) ++ ", but expected: " ++ (toS expected)
+bodyBeing :: ByteString -> ResponseMatcher
+bodyBeing expected = let status = 200
+                         match _ body | body == expected = Nothing
+                                      | otherwise = Just $ "Got: " ++ (toS body) ++ ", but expected: " ++ (toS expected)
+                     in status {matchBody = MatchBody match}
+
+shouldHaveBody :: WaiSession st SResponse -> ByteString -> WaiExpectation st
+shouldHaveBody r e = r `shouldRespondWith` bodyBeing e
 
 spec :: Spec
 spec = with (return app) $ do
@@ -25,10 +28,6 @@ spec = with (return app) $ do
         it "responds with 200" $ do
             get "/estimations" `shouldRespondWith` 200
         it "responds with [Estimation]" $ do
-            let
-              estimations = encode
-                [ Estimation 1
-                , Estimation 3
-                ]
-              result = get "/estimations"
-            result `shouldRespondWith` 200 {matchBody = withBody estimations}
+            get "/estimations" `shouldHaveBody` encode [ Estimation 1
+                                                       , Estimation 2
+                                                       ]
